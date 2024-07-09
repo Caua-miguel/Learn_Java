23889528494c7be75ac18412cc7fa80416db8ccc
Cauã Miguel Sudol Rocha <132935686+Caua-miguel@users.noreply.github.com>
2024-07-09 11:43:47 -0300

    learn JUnit

---
+++ src/test/java/firstTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class firstTest {
+    @Test
+    public void testJUnit(){
+        Assertions.assertTrue(true);
+    }
+
+}
