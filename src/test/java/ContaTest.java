827513b4fbc235f8b2c84da2e3f5c66054d17506
Cauã Miguel Sudol Rocha <132935686+Caua-miguel@users.noreply.github.com>
2024-07-09 16:16:14 -0300

    JUnit: use BeforeAll

--- src/test/java/ContaTest.java
+++ src/test/java/ContaTest.java
@@ -1,12 +1,22 @@
 import me.cauadeveloper.learn_tdd.Conta;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class ContaTest {
 
     private Conta conta;
+
+    private static double  depositoInicial, saldoZerado, saqueValido, saqueInvalido;
+
+    @BeforeAll
+    public static void definirValoresPadrao(){
+
+        saldoZerado = 0.00;
+        depositoInicial = 100.00;
+        saqueValido = 70.00;
+        saqueInvalido = 110.00;
+
+    }
+
 
     // Executa antes de cada método
     @BeforeEach
@@ -20,10 +30,10 @@ public class ContaTest {
     @DisplayName("Deve Depositar Com Conta Ativa")
     public void deveDepositarComContaAtiva(){
 
-        conta.depositar(100.00);
+        conta.depositar(depositoInicial);
 
         // Primeiro valor é o esperado o segundo é o obtido
-        Assertions.assertEquals(100.00, conta.getSaldo());
+        Assertions.assertEquals(depositoInicial, conta.getSaldo());
 
     }
 
@@ -32,10 +42,10 @@ public class ContaTest {
     public void naoDeveDepositarComContaInativa(){
 
         conta.inativar();
-        conta.depositar(100.00);
+        conta.depositar(depositoInicial);
 
         // Primeiro valor é o esperado o segundo é o obtido
-        Assertions.assertEquals(0.00, conta.getSaldo());
+        Assertions.assertEquals(saldoZerado, conta.getSaldo());
 
     }
 
@@ -43,11 +53,11 @@ public class ContaTest {
     @DisplayName("Deve Sacar Com Conta Ativa e Saldo Maior que o Valor de Saque")
     public void deveSacarComContaAtivaESaldoMaiorQueValorSaque(){
 
-        conta.depositar(100.00);
-        conta.sacar(70.00);
+        conta.depositar(depositoInicial);
+        conta.sacar(saqueValido);
 
         // Primeiro valor é o esperado o segundo é o obtido
-        Assertions.assertEquals(30.00, conta.getSaldo());
+        Assertions.assertEquals(depositoInicial - saqueValido, conta.getSaldo());
 
     }
 
@@ -55,11 +65,11 @@ public class ContaTest {
     @DisplayName("Não Deve Sacar Com Conta Ativa e Saldo Menor que o Valor de Saque")
     public void naoDeveSacarComContaAtivaESaldoMenorQueValorSaque(){
 
-        conta.depositar(100.00);
-        conta.sacar(110.00);
+        conta.depositar(depositoInicial);
+        conta.sacar(saqueInvalido);
 
         // Primeiro valor é o esperado o segundo é o obtido
-        Assertions.assertEquals(100.00, conta.getSaldo());
+        Assertions.assertEquals(depositoInicial, conta.getSaldo());
 
     }
 
@@ -67,13 +77,13 @@ public class ContaTest {
     @DisplayName("Não Deve Sacar Com Conta Inativa")
     public void naoDeveSacarComContaInativa(){
 
-        conta.depositar(100.00);
+        conta.depositar(depositoInicial);
 
         conta.inativar();
-        conta.sacar(70.00);
+        conta.sacar(saqueValido);
 
         // Primeiro valor é o esperado o segundo é o obtido
-        Assertions.assertEquals(100.00, conta.getSaldo());
+        Assertions.assertEquals(depositoInicial, conta.getSaldo());
 
     }
 
